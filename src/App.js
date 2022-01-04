46355d461fe38cb9701e6bb4914a21957dbd093b
VictoriaCardos <vvjcerque@hotmail.com>
2022-01-04 19:56:56 -0300

    feat: add header

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import { BrowserRouter as Router } from 'react-router-dom'
 import './App.css'
+import Header from './components/header/Header'
 
 import Routes from './routes'
 
 function App() {
   return (
     <Router>
+      <Header>Pizza Place</Header>
       <Routes />
     </Router>
   )
