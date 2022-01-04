f89df61dd354eaa1b9eaacf46d6132947fed60c0
VictoriaCardos <vvjcerque@hotmail.com>
2022-01-03 23:50:10 -0300

    feat: add path not found

--- src/routes.js
+++ src/routes.js
@@ -11,6 +11,9 @@ export default function MainRoutes() {
       <Route path="/" element={<Home />} />
       <Route path="/sobre" element={<Sobre />} />
       <Route path="/cadastro" element={<Cadastro />} />
+      // o * significa que quando existir uma rota que não existe ele vai dizer
+      que a página não foi encontrada
+      <Route path="*" element={<h1>Not Found</h1>} />
     </Routes>
   )
 }
