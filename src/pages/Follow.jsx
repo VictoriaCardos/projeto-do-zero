89ed800ca82d59789b156a71a181da7a5be44e2f
VictoriaCardos <vvjcerque@hotmail.com>
2022-01-08 17:28:05 -0300

    feat: add button voltar

--- src/pages/Follow.jsx
+++ src/pages/Follow.jsx
@@ -1,4 +1,6 @@
 import React, { useContext } from 'react'
+import { useNavigate } from 'react-router-dom'
+import Button from '../components/button/Button'
 
 import Container from '../components/container'
 import FollowContainer from '../components/followContainer'
@@ -7,9 +9,11 @@ import { context } from '../context'
 
 const Follow = () => {
   const ctx = useContext(context)
+  const navigate = useNavigate()
 
   return (
     <Container>
+      <Button onClick={() => navigate('/')}>VOLTAR</Button>
       <FollowContainer name={ctx.userData?.name} followers={ctx.followers} />
     </Container>
   )
