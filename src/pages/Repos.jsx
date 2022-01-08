0649b9a7d26410fdb4b72d6d4f944792e4609fcd
VictoriaCardos <vvjcerque@hotmail.com>
2022-01-08 17:28:43 -0300

    feat: add button voltar

--- src/pages/Repos.jsx
+++ src/pages/Repos.jsx
@@ -1,4 +1,6 @@
 import React, { useContext } from 'react'
+import { useNavigate } from 'react-router-dom'
+import Button from '../components/button/Button'
 
 import Container from '../components/container'
 import ReposContainer from '../components/reposContainer'
@@ -7,9 +9,11 @@ import { context } from '../context'
 
 const Repos = () => {
   const ctx = useContext(context)
+  const navigate = useNavigate()
 
   return (
     <Container>
+      <Button onClick={() => navigate('/')}>Voltar</Button>
       <ReposContainer name={ctx.userData?.name} repos={ctx.repos} />
     </Container>
   )
