4facb0c6bac839a27aefc4058d51617d08d504a6
VictoriaCardos <vvjcerque@hotmail.com>
2022-01-08 17:29:29 -0300

    feat: create route for following page

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Routes, Route } from 'react-router-dom'
 import Follow from './pages/Follow'
+import Following from './pages/Following'
 import Home from './pages/Home'
 import Repos from './pages/Repos'
 
@@ -10,6 +11,7 @@ export default function MainRoutes() {
       <Route path="/" element={<Home />} />
       <Route path="/repos" element={<Repos />} />
       <Route path="/follow" element={<Follow />} />
+      <Route path="/following" element={<Following />} />
     </Routes>
   )
 }
