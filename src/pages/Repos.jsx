224b0e360797349a8a18cec2c5faa77c95ca1167
VictoriaCardos <vvjcerque@hotmail.com>
2022-01-09 21:39:19 -0300

    feat: add icon go back

--- src/pages/Repos.jsx
+++ src/pages/Repos.jsx
@@ -1,5 +1,6 @@
 import React, { useContext } from 'react'
 import { useNavigate } from 'react-router-dom'
+import { FiCornerDownLeft } from 'react-icons/fi'
 import Button from '../components/button/Button'
 
 import Container from '../components/container'
@@ -13,7 +14,9 @@ const Repos = () => {
 
   return (
     <Container>
-      <Button onClick={() => navigate('/')}>Voltar</Button>
+      <Button onClick={() => navigate('/')}>
+        <FiCornerDownLeft size={15} />
+      </Button>
       <ReposContainer name={ctx.userData?.name} repos={ctx.repos} />
     </Container>
   )
