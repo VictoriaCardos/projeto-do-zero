6427ff3ae750ae9bce9c0ba96d61e45d21bbd608
VictoriaCardos <vvjcerque@hotmail.com>
2022-01-09 21:38:55 -0300

    feat: add icon go back

--- src/pages/Following.jsx
+++ src/pages/Following.jsx
@@ -1,5 +1,6 @@
 import React, { useContext } from 'react'
 import { useNavigate } from 'react-router-dom'
+import { FiCornerDownLeft } from 'react-icons/fi'
 import Button from '../components/button/Button'
 
 import Container from '../components/container'
@@ -13,7 +14,9 @@ const Following = () => {
 
   return (
     <Container>
-      <Button onClick={() => navigate('/')}>VOLTAR</Button>
+      <Button onClick={() => navigate('/')}>
+        <FiCornerDownLeft size={15} />
+      </Button>
       <FollowingContainer name={ctx.userData?.name} following={ctx.following} />
     </Container>
   )
