1e117030ced84770dd0167907286bc60508ca86e
VictoriaCardos <vvjcerque@hotmail.com>
2022-01-11 22:37:03 -0300

    style: new heigth

--- src/components/container/styles.js
+++ src/components/container/styles.js
@@ -1,9 +1,7 @@
 import styled from 'styled-components'
 
 export const Container = styled.main`
-  width: 100%;
-  height: 100vh;
-  background: linear-gradient(120deg, #252525, #444);
+  height: 85vh;
   display: flex;
   align-items: center;
   justify-content: center;
