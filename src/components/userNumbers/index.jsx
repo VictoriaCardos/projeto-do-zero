ef88a574a950cf39568efde6663ef67e2d17127c
VictoriaCardos <vvjcerque@hotmail.com>
2022-01-11 22:37:43 -0300

    style: remove text decoration

--- src/components/userNumbers/index.jsx
+++ src/components/userNumbers/index.jsx
@@ -5,20 +5,20 @@ import { Link } from 'react-router-dom'
 const UserNumbers = props => {
   return (
     <Container>
-      <Link to="/repos">
+      <Link to="/repos" style={{ textDecoration: 'none' }}>
         <NumberContainer>
           <h1>{props.repos}</h1>
           <h2>Repositórios</h2>
         </NumberContainer>
       </Link>
-      <Link to="/follow">
+      <Link to="/follow" style={{ textDecoration: 'none' }}>
         <NumberContainer>
           <h1>{props.followers}</h1>
           <h2>Seguidores</h2>
         </NumberContainer>
       </Link>
 
-      <Link to="following">
+      <Link to="following" style={{ textDecoration: 'none' }}>
         <NumberContainer>
           <h1>{props.following}</h1>
           <h2>Seguindo</h2>
