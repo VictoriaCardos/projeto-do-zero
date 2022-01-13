e89dd942a70858d63091fe0f5e41a32b85a6f6a8
VictoriaCardos <vvjcerque@hotmail.com>
2022-01-12 23:41:43 -0300

    style: import css

--- src/components/button/Button.jsx
+++ src/components/button/Button.jsx
@@ -1,7 +1,12 @@
 import React from 'react'
+import './Button.css'
 
 const Button = props => {
-  return <button onClick={props.onClick}>{props.children}</button>
+  return (
+    <button className="goBack" onClick={props.onClick}>
+      {props.children}
+    </button>
+  )
 }
 
 export default Button
